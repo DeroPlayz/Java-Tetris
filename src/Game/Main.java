615a5629173cffce405b1b929ccbe94f3cfcb111
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2023-12-18 19:06:56 +0000

    Created main.

---
+++ src/Game/Main.java
@@ -0,0 +1,11 @@
+package Game;
+
+class Main{
+    static JFrame frame = new JFrame("Tetris");
+    static JButton play = new JButton("Play");
+    static JButton settings = new JButton("Settings");
+    static JButton exit = new JButton("Exit to Desktop.");
+    public static void main(String[] args) {
+        frame.setVisible(true);
+    }
+}
