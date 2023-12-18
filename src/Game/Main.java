afeae8d9a2d7dd33b8dcafc61b6e7f6ec996236b
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2023-12-18 19:24:48 +0000

    too many to commit bc im lazy (100_emoji)

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,11 +1,21 @@
 package Game;
 
+import javax.swing.*;
+
+import Game.Tetrimino.*;
+import static Game.Tetrimino.Tetrimino.*;
+
 class Main{
+    static Tetrimino currentPiece;
+    static final Tetrimino[] classicBag = {I_Piece, T_Piece, O_Piece, S_Piece, Z_Piece, L_Piece, J_Piece};
     static JFrame frame = new JFrame("Tetris");
     static JButton play = new JButton("Play");
     static JButton settings = new JButton("Settings");
     static JButton exit = new JButton("Exit to Desktop.");
     public static void main(String[] args) {
+        frame.setAutoRequestFocus(true);
         frame.setVisible(true);
+        frame.setSize(1000, 800);
+        
     }
 }
---
+++ src/Game/Tetrimino/Tetrimino.java
@@ -0,0 +1,25 @@
+package Game.Tetrimino;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.IIOImage;
+import javax.swing.ImageIcon;
+
+public class Tetrimino {
+    String name;
+    ImageIcon texture;
+
+    public Tetrimino(String name, ImageIcon texture){
+        this.name = name;
+        this.texture = texture;
+    }
+
+    public static final Tetrimino I_Piece = new Tetrimino("I", new ImageIcon("res/I_Piece.png"));
+    public static final Tetrimino T_Piece = new Tetrimino("T", new ImageIcon("res/T_Piece.png"));
+    public static final Tetrimino O_Piece = new Tetrimino("O", new ImageIcon("res/O_Piece.png"));
+    public static final Tetrimino S_Piece = new Tetrimino("S", new ImageIcon("res/S_Piece.png"));
+    public static final Tetrimino Z_Piece = new Tetrimino("Z", new ImageIcon("res/Z_Piece.png"));
+    public static final Tetrimino L_Piece = new Tetrimino("L", new ImageIcon("res/L_Piece.png"));
+    public static final Tetrimino J_Piece = new Tetrimino("J", new ImageIcon("res/J_Piece.png"));
+}
